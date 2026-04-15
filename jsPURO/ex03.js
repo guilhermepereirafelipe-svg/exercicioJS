2049cd3c1ba59ae3d866b4fd3b2d6c184011aca9
guilhermepereirafelipe-svg <guilherme.pereira.felipe@escola.pr.gov.br>
2026-04-15 17:09:30 -0300

    Add files via upload

---
+++ jsPURO/ex03.js
@@ -0,0 +1,13 @@
+let nome = 'Pereira';
+console.log(nome);
+
+let cidade = 'maringa';
+let ano = 2010;
+console.log(`Eu moro em ${cidade} e nasci no ano ${ano}`);
+
+let idade = 16; 
+if (idade >= 18) {
+    console.log('pode dirigir')
+}else {
+    console.log('não pode')
+}
---
+++ jsPURO/ex06.js
@@ -0,0 +1,19 @@
+//foreach 
+let lista = ['Pereira', 16, 'Maçã', true];
+console.log(lista);
+console.log(lista[1]);
+ lista.push('Pereira');
+ console.log(lista);
+ lista.unshift('Pera'); //adiciona no começo da lista
+ console.log(lista);
+ lista.pop(); 
+ console.log(lista);
+ lista.shift();
+ console.log(lista);
+ lista.reverse(); //inverter a ordem da lista
+ console.log(lista);
+ console.log(lista.length) //mostra o 'comprimento' da lista
+ lista.push([2] 'josé');
+ console.log(lista);
+ lista.splice(1, 0 'Carlos'); //adiciona no meio da lista
+ console.log(lista);
